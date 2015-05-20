a18cddcaf68583b2139bd98c70f4bb03f656b9a9
hiephuynhngoc <mr.hiepuit@gmail.com>
2015-05-20 15:14:37 +0700

    init Project

---
+++ instaPic/instaPic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  instaPic
+//
+//  Created by HiepHN-imac on 5/20/15.
+//  Copyright (c) 2015 Hiep Huynh Ngoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ instaPic/instaPic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  instaPic
+//
+//  Created by HiepHN-imac on 5/20/15.
+//  Copyright (c) 2015 Hiep Huynh Ngoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
